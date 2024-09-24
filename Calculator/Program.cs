4f7402856e4c1ce4d5765aad16b9456525d3510f
benjamincolhoun <Colhoun-B2@ulster.ac.uk>
2024-09-24 14:33:06 +0100

    update

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,3 +3,4 @@ int secondNumber = 20;
 int result = firstNumber + secondNumber;
 Console.WriteLine("The addition of both number is {0}",result);
 Console.ReadKey();
+Console.WriteLine("The addition of both number is {0}", result);
